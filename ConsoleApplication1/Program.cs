bf66612b734dce3e21d64bf4ca88d94333724fcc
rishikamaroo <rishikamaroo@gmail.com>
2016-11-04 19:39:58 +0000

    g

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,8 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!!");
-            Console.ReadLine();
-           
+            Console.ReadLine();         
         }
     }
 }
